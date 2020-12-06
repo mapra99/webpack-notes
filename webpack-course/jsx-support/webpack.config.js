46b24fdbc7429be359ccab67b3ee7afacf58368f
Miguel Prada <mapra99@gmail.com>
2020-12-06 13:44:43 -0500

    react support

---
+++ webpack-course/jsx-support/webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    home: path.resolve(__dirname, 'src/js/index.js')
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/[name].js'
+  },
+  devServer: {
+    hot: true,
+    open: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /^node_modules$/,
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Webpack Dev Server'
+    }),
+
+    new webpack.HotModuleReplacementPlugin()
+  ]
+}
